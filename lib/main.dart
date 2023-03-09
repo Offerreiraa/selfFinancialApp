b002c7019f8797a299d240a042d0e977839f5a80
Gabriel Ferreira da Silva <101992592+Offerreiraa@users.noreply.github.com>
2023-03-09 09:18:12 -0300

    Creating an showModalBottomSheet for the floatingButton, changing main to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'components/transaction_form.dart';
+import 'components/transaction_list.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(const ExpensesApp());
 
@@ -14,8 +17,61 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transaction = [
+    Transaction(
+      id: "t1",
+      title: "Tênis novo",
+      value: 350.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: "Engradado de Monster",
+      value: 60.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t3",
+      title: "Engradado de Monster",
+      value: 60.00,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t4",
+      title: "Engradado de Monster",
+      value: 60.00,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transaction.add(newTransaction);
+    });
+  }
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm(_addTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +80,7 @@ class MyHomePage extends StatelessWidget {
         title: const Text("Expenses App"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () => _openTransactionFormModal(context),
             icon: const Icon(Icons.add),
           ),
         ],
@@ -32,8 +88,8 @@ class MyHomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch, //Flexbox eixo x
-          children: const [
-            SizedBox(
+          children: [
+            const SizedBox(
               width: double.infinity,
               child: Card(
                 color: Colors.blue,
@@ -41,14 +97,13 @@ class MyHomePage extends StatelessWidget {
                 child: Text("Gráfico"),
               ),
             ),
-            // TransactionList(transaction: _transaction),
-            TransactionUser(),
+            TransactionList(transaction: _transaction),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () => _openTransactionFormModal(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
